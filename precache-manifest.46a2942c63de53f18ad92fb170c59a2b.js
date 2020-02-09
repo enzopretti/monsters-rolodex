d2a61ce6b8019b831112d212a0ebe5b6646b8d72
enzopretti <enzopretti@hotmail.com>
2020-02-09 22:25:12 +0000

    Updates

---
+++ precache-manifest.46a2942c63de53f18ad92fb170c59a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a480845461f0f22b3c85ec96693e1fe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec7f2370c918de593888",
+    "url": "/monsters-rolodex/static/css/main.dce80e4f.chunk.css"
+  },
+  {
+    "revision": "08ece6ebcdc2f672491a",
+    "url": "/monsters-rolodex/static/js/2.b0983705.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.b0983705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7f2370c918de593888",
+    "url": "/monsters-rolodex/static/js/main.261515b6.chunk.js"
+  },
+  {
+    "revision": "74fe9f909a110daa43a5",
+    "url": "/monsters-rolodex/static/js/runtime-main.f0774556.js"
+  }
+]);
